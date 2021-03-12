62f619de0496518341fbee0a13a59a6a3ecbb3ae
Rejwan Islam <50569315+RIR360@users.noreply.github.com>
2021-03-12 17:32:10 +0600

    add animation 2

---
+++ code/animation1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include "drawrect.h"
+
+int main()
+{
+    rect canvas = {0, 0, 20, 20};
+    int i;
+
+    while (1)
+    {
+        for (i = 0; i < 15; i++)
+        {
+            drawRect(i, i, 5, 5, canvas);
+            system("cls");
+        }
+        for (i = 5; i < 20; i++)
+        {
+            drawRect(20 - i, 20 - i, 5, 5, canvas);
+            system("cls");
+        }
+    }
+    getch();
+    return 0;
+}
